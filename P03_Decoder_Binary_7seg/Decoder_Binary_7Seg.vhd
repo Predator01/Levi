d9619c301ed622cf5e48c35e3f1aa908125afaf8
Predator01 <miguel.agr92@gmail.com>
2015-08-30 17:34:33 -0500

    Decoder process if

--- P03_Decoder_Binary_7seg/Decoder_Binary_7Seg.vhd
+++ P03_Decoder_Binary_7seg/Decoder_Binary_7Seg.vhd
@@ -77,36 +77,75 @@ begin
 --			 "10000110" when Ent="1110" else --14 E
 --			 "10001110"; --15 F
 --	--select display
-
+--			Disp <= "1110";
 			
 	-- Behavioral Concurrent implementation using select / when
 	-- Using embedded signal
 	
+--	Ent <= D & C & B & A;
+--   with Ent select
+--      Seg <= "11000000" when "0000",
+--				"11111001" when "0001", --1
+--				"10100100" when "0010", --2
+--				"10110000" when "0011", --3 
+--				"10011001" when "0100", --4
+--				"10010010" when "0101", --5
+--				"10000010" when "0110", --6
+--				"11111000" when "0111", --7
+--				"10000000" when "1000", --8
+--				"10011000" when "1001", --9
+--				"10001000" when "1010", --10 A
+--				"10000011" when "1011", --11 B
+--				"11000110" when "1100", --12 C
+--				"10100001" when "1101", --13 D
+--				"10000110" when "1110", --14 E
+--				"10001110" when others ; --15 F
+--	
+--	Disp <= "1110";
+	
+	--- Sequential Process
+	
 	Ent <= D & C & B & A;
-   with Ent select
-      Seg <= "11000000" when "0000",
-				"11111001" when "0001", --1
-				"10100100" when "0010", --2
-				"10110000" when "0011", --3 
-				"10011001" when "0100", --4
-				"10010010" when "0101", --5
-				"10000010" when "0110", --6
-				"11111000" when "0111", --7
-				"10000000" when "1000", --8
-				"10011000" when "1001", --9
-				"10001000" when "1010", --10 A
-				"10000011" when "1011", --11 B
-				"11000110" when "1100", --12 C
-				"10100001" when "1101", --13 D
-				"10000110" when "1110", --14 E
-				"10001110" when others ; --15 F
- 
-			
-
+	process (Ent)
+		VARIABLE bufer: STD_LOGIC_VECTOR (7 downto 0);
+	begin
+		if Ent = "0000" then
+			bufer := "11000000"; --0
+		elsif Ent = "0001" then 
+			bufer := "11111001"; -- 1
+		elsif Ent = "0010" then 
+			bufer := "10100100"; --2
+		elsif Ent = "0011" then 
+			bufer := "10110000"; --3 
+		elsif Ent = "0100" then 
+			bufer := "10011001"; --4
+		elsif Ent = "0101" then 
+			bufer := "10010010"; --5
+		elsif Ent = "0110" then 
+			bufer := "10000010"; --6
+		elsif Ent = "0111" then 
+			bufer := "11111000"; --7
+		elsif Ent = "1000" then 
+			bufer := "10000000"; --8
+		elsif Ent = "1001" then 
+			bufer := "10011000"; --9
+		elsif Ent = "1010" then 
+			bufer := "10001000"; --10 A
+		elsif Ent = "1011" then 
+			bufer := "10000011"; --11 B
+		elsif Ent = "1100" then 
+			bufer := "11000110"; --12 C
+		elsif Ent = "1101" then 
+			bufer := "10100001"; --13 D
+		elsif Ent = "1110" then 
+			bufer := "10000110"; --14 E
+		else
+			bufer := "10001110"; --15 F
+	  end if;
+	  Seg <= bufer;
+	end process;
 	
 	Disp <= "1110";
-	
-	
 	
 				 
 
